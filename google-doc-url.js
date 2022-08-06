5c11db9f0efeb15c9816fc44b3f3e5436f718aee
repairmap <106491583+repairmap@users.noreply.github.com>
2022-08-06 15:45:05 -0700

    Update google-doc-url.js
    updated with personal API key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/17gRluFQ755wXCoWezLsNkqGjmrdzDP1oXP3z1HacAm4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCgLmd2NtkuH9kJhVKY6HpUE-i9sTIlJM4';
